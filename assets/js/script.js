d76b7e7b64a756845300fe12d2c58b2502ae7531
Shashikanta Mahato <shashikanta2234@gmail.com>
2023-08-24 17:17:47 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,42 @@
-
 /* Nave menu */
-const nav_class=document.querySelector(".nav");
-const showNavbar= () =>{
-    nav_class.classList.toggle('active');
-}
+const nav_class = document.querySelector(".nav");
+const showNavbar = () => {
+  nav_class.classList.toggle("active");
+};
 
 const mobile_nav = document.querySelector(".nav-btn");
-mobile_nav.addEventListener('click',()=>showNavbar());
+mobile_nav.addEventListener("click", () => showNavbar());
 
-const nav_content=document.querySelector('.nav-content');
-nav_content.addEventListener('mouseout',()=>showNavbar());
+const nav_content = document.querySelector(".nav-content");
+nav_content.addEventListener("mouseout", () => showNavbar());
+
+/* Owl Carousel */
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items:5,
+    center:true,
+    margin:5,
+    autoplay:2000,
+    pagination:true,
+    mergeFit:true,
+    rewind:true,
+    autoplayHoverPause:true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      500: {
+        items: 2,
+      },
+      680: {
+        items: 3,
+      },
+      840: {
+        items: 5,
+      },
+    },
+  });
+});
